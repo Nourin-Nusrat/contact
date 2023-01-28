ec4d87798fafd45e4aaa5b21c0bd512ec8d4813d
Nourin-Nusrat <sarder.nou@gmail.com>
2023-01-28 10:49:23 +0600

    commit

---
+++ assets/main.js
@@ -0,0 +1,31 @@
+function sendEmail(){
+    Email.send({
+        Host : "smtp.elasticemail.com",
+        Username : "username",
+        Password : "password",
+        To : 'them@website.com',
+        From : "you@isp.com",
+        Subject : "This is the subject",
+        Body : "And this is the body"
+    }).then(
+      message => alert(message)
+    );
+}
+
+
+const inputs = document.querySelectorAll(".input");
+
+function focusFunc(){
+        let parent = this.parentNode;
+        parent.classList.add("focus");
+}
+function blurFunc(){
+    let parent = this.parentNode;
+    parent.classList.remove("focus");
+}
+
+inputs.forEach(input=>{
+    input.addEventListener("focus",focusFunc);
+    input.addEventListener("blur",focusFunc);
+})
+
